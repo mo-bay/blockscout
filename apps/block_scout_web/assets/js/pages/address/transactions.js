aed338f44302365175026fdec001f971ff80fb20
nikitosing <nikitosing4@mail.ru>
2021-07-15 03:05:41 +0700

    initial does not work

--- apps/block_scout_web/assets/js/pages/address/transactions.js
+++ apps/block_scout_web/assets/js/pages/address/transactions.js
@@ -5,8 +5,11 @@ import humps from 'humps'
 import { subscribeChannel } from '../../socket'
 import { connectElements } from '../../lib/redux_helpers.js'
 import { createAsyncLoadStore } from '../../lib/async_listing_load'
+import { batchChannel } from '../lib/utils'
 import '../address'
 import { isFiltered } from './utils'
+
+const BATCH_THRESHOLD = 6
 
 export const initialState = {
   addressHash: null,
@@ -28,13 +31,47 @@ export function reducer (state, action) {
     case 'RECEIVED_NEW_TRANSACTION': {
       if (state.channelDisconnected) return state
 
-      if (state.beyondPageOne ||
-        (state.filter === 'to' && action.msg.toAddressHash !== state.addressHash) ||
-        (state.filter === 'from' && action.msg.fromAddressHash !== state.addressHash)) {
-        return state
+      /*if (state.beyondPageOne ||
+        (state.filter === 'to' && action.msgs.toAddressHash !== state.addressHash) ||
+        (state.filter === 'from' && action.msgs.fromAddressHash !== state.addressHash)) {
+          return state
+      }*/
+
+      const transactionCount = state.transactionCount + action.msgs.length
+
+      if (state.transactionsLoading || state.transactionsError) {
+        return Object.assign({}, state, { transactionCount })
       }
 
-      return Object.assign({}, state, { items: [action.msg.transactionHtml, ...state.items] })
+      const transactionsLength = state.transactions.length + action.msgs.length
+      if (transactionsLength < BATCH_THRESHOLD) {
+        return Object.assign({}, state, {
+          transactions: [
+            ...action.msgs.reverse(),
+            ...state.transactions
+          ],
+          transactionCount
+        })
+      } else if (!state.transactionsBatch.length && action.msgs.length < BATCH_THRESHOLD) {
+        return Object.assign({}, state, {
+          transactions: [
+            ...action.msgs.reverse(),
+            ...state.transactions.slice(0, -1 * action.msgs.length)
+          ],
+          transactionCount
+        })
+      } else {
+        return Object.assign({}, state, {
+          transactionsBatch: [
+            ...action.msgs.reverse(),
+            ...state.transactionsBatch
+          ],
+          transactionCount
+        })
+      }
+
+
+     // return Object.assign({}, state, { items: [action.msg.transactionHtml, ...state.items] })
     }
     case 'RECEIVED_NEW_REWARD': {
       if (state.channelDisconnected) return state
@@ -66,6 +103,14 @@ const elements = {
 
       return $el.show()
     }
+  },
+  '[data-selector="channel-batching-count"]': {
+    render ($el, state, _oldState) {
+      const $channelBatching = $('[data-selector="channel-batching-message"]')
+      if (!state.transactionsBatch.length) return $channelBatching.hide()
+      $channelBatching.show()
+      $el[0].innerHTML = numeral(state.transactionsBatch.length).format()
+    }
   }
 }
 
@@ -85,18 +130,18 @@ if ($('[data-page="address-transactions"]').length) {
 
   const addressChannel = subscribeChannel(`addresses:${addressHash}`)
   addressChannel.onError(() => store.dispatch({ type: 'CHANNEL_DISCONNECTED' }))
-  addressChannel.on('transaction', (msg) => {
+  addressChannel.on('transaction', batchChannel((msgs) => 
     store.dispatch({
       type: 'RECEIVED_NEW_TRANSACTION',
-      msg: humps.camelizeKeys(msg)
+      msgs: humps.camelizeKeys(msgs)
     })
-  })
-  addressChannel.on('pending_transaction', (msg) => {
+  ))
+  addressChannel.on('pending_transaction', batchChannel((msgs) => 
     store.dispatch({
       type: 'RECEIVED_NEW_TRANSACTION',
-      msg: humps.camelizeKeys(msg)
+      msgs: humps.camelizeKeys(msgs)
     })
-  })
+  ))
 
   const rewardsChannel = subscribeChannel(`rewards:${addressHash}`)
   rewardsChannel.onError(() => store.dispatch({ type: 'CHANNEL_DISCONNECTED' }))
@@ -104,6 +149,17 @@ if ($('[data-page="address-transactions"]').length) {
     store.dispatch({
       type: 'RECEIVED_NEW_REWARD',
       msg: humps.camelizeKeys(msg)
+    })
+  })
+
+  const $txReloadButton = $('[data-selector="reload-transactions-button"]')
+  const $channelBatching = $('[data-selector="channel-batching-message"]')
+  $txReloadButton.on('click', (event) => {
+    event.preventDefault()
+    loadTransactions(store)
+    $channelBatching.hide()
+    store.dispatch({
+      type: 'TRANSACTION_BATCH_EXPANDED'
     })
   })
 }
